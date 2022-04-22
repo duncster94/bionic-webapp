de2efa5c930994d179c6af7befabce88b83bf67e
duncster94 <duncan.forster@mail.utoronto.ca>
2022-04-22 12:14:45 -0400

    remove python process

--- server/index.js
+++ server/index.js
@@ -5,25 +5,27 @@ const app = express();
 
 const yeastUniprotDescriptions = require("./utils/uniprot-yeast-descriptions.json");
 const cyto = require("./features/bionic_net_0.5.txt.json");
+const tsneResults = require("./features/yeast-BIONIC-data.json");
 
 app.use(cors());
 
 app.get("/tsne", (req, res) => {
-  const ls = spawn("python", [
-    "compute-tsne.py",
-    "features/yeast_BIONIC_features.csv",
-  ]);
+  // const ls = spawn("python", [
+  //   "compute-tsne.py",
+  //   "features/yeast_BIONIC_features.csv",
+  // ]);
 
-  ls.stdout.on("data", (data) => {
-    data = JSON.parse(data);
-    res.json(data);
-  });
+  // ls.stdout.on("data", (data) => {
+  //   data = JSON.parse(data);
+  //   res.json(data);
+  // });
 
-  ls.stderr.on("data", (data) => {
-    console.log(`stderr: ${data}`);
-  });
+  // ls.stderr.on("data", (data) => {
+  //   console.log(`stderr: ${data}`);
+  // });
 
-  ls.on("close", (code) => {});
+  // ls.on("close", (code) => {});
+  res.json(tsneResults);
 });
 
 app.get("/", (req, res) => {
@@ -43,4 +45,5 @@ app.get("/network", (req, res) => {
   res.json({ cytoPos: pos, cytoEdges: edges });
 });
 
-app.listen(4000);
+const port = process.env.PORT || 4000;
+app.listen(port);
